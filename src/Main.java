24b5daf26e7b7f703a4b1a05a3917f7316f4ae57
RicardoMaranha <ricardomaranhapro@outlook.com>
2024-12-30 11:14:15 -0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        int maiorIdade = 18;
+        int idosoIdade = 60;
+        int crianca = 6;
+
+        System.out.println("Maior idade: " + maiorIdade);
+        System.out.println("Criança idade: " + crianca);
+        System.out.println("Idoso idade: " + idosoIdade);
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
