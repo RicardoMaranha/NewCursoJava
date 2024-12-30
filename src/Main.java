1cf941a4889099bde5d34942bc0d7c567e09f8e0
RicardoMaranha <ricardomaranhapro@outlook.com>
2024-12-30 11:50:21 -0300

    exercício1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static int idadeGlobal = 18;
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -19,5 +21,10 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        metodo2();
+    }
+
+    public static void metodo2() {
+        System.out.println("Idade Global: " + idadeGlobal);
     }
 }
