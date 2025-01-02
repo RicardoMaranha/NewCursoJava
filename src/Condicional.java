afbf2ccfe88ffbdf532275433f19af572d362303
RicardoMaranha <ricardomaranhapro@outlook.com>
2025-01-02 09:35:21 -0300

    condicional

---
+++ src/Condicional.java
@@ -0,0 +1,13 @@
+public class Condicional {
+    public static void main(String[] args) {
+        char sexo = 'M';
+
+        if(sexo == 'F'){
+            System.out.println("Sexo: Feminino" );
+        } else if(sexo == 'M') {
+            System.out.println("Sexo: Masculino");
+        } else {
+            System.out.println("Sexo: Valor inválido");
+        }
+    }
+}
---
+++ src/Variaveis.java
@@ -0,0 +1,30 @@
+public class Variaveis {
+
+    public static void  main(String[] args){
+        /*Tipo double, para numeros com ponto flutuante de alta precisão */
+        double nota1 = 10;
+        double nota2 = 8;
+        double nota3 = 4;
+        double nota4 = 8;
+        double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Sua nota média é: " + mediaFinal);
+        /*Tipo char - para apenas um character*/
+        char sexo = 'M';
+
+        System.out.println(sexo);
+
+        if(sexo == 'F'){
+            System.out.println("Sexo feminino!");
+        }
+        else if(sexo == 'M'){
+            System.out.println("Sexo masculino!");
+        }
+        else{
+            System.out.println("Valor inválido!");
+        }
+
+        /*Tipo String, para textos*/
+               
+    }
+}
