599d2cf43fc71475da3e82fd28a0be82835ecd15
RicardoMaranha <ricardomaranhapro@outlook.com>
2025-01-02 15:29:08 -0300

    EstruturaRepeticao2

--- src/EstruturaRepeticao.java
+++ src/EstruturaRepeticao.java
@@ -15,5 +15,32 @@ public class EstruturaRepeticao {
         } while (numero >= 0);
         System.out.println(numero);
 
+        /* for */
+
+        for(int i=0; i<=10; i++){
+            System.out.println("Indice: " + i);
+        }
+
+        /* for + break */
+        int numeroBusca = 1;
+        System.out.println("Numero de busca: " + numeroBusca);
+        for(int i = 10; i >= 0; i--){
+            System.out.println("Indice: " + i);
+            if(i == numeroBusca){
+                System.out.println("Encontrei o numero: " + numeroBusca);
+                System.out.println("Estou parando de executar");
+                break;
+            }
+        }
+
+        /* for + continue */
+        System.out.println("for + continue: ");
+        for(int i = 0; i<= 10; i++){
+            System.out.println("Indice: " + i);
+            if (i == 5 || i == 8 || i == 10){
+                System.out.println("Encontrei o numero: " + i);
+                continue;
+            }
+        }
     }
 }
